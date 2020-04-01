efaa13d76f0d4fd05a97568337a180a0de757a4a
Mark Lopez <m@silvenga.com>
2020-03-31 20:26:04 -0500

    Added basic project structure.

---
+++ src/Swagit.Scraper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Swagit.Scraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
